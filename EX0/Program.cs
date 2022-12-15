e7a7d2453ef6bef2b6391b6ea455b6c2b8ff5168
Electronic <sv_gravity100@mail.ru>
2022-12-15 23:31:02 +0300

    программа, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя)

---
+++ EX0/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write($"Введите любое целое число: ");
+string input = Console.ReadLine() ??"";
+
+int number = int.Parse(input);
+double result = Math.Pow(number, 2);
+
+Console.WriteLine(result);  
+
+
+
+
+
+
+
+
+
+
+
+
