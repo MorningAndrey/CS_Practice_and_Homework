4b20e4ae3219894d876a0a3f44d16c4b7f3867f4
Electronic <sv_gravity100@mail.ru>
2022-12-15 23:42:57 +0300

    программа, которая выдает название дня недели по заданному номеру

---
+++ EX1/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write($"Введите любое целое число от 1 до 7: ");
+string input = Console.ReadLine() ??"";
+
+int number = int.Parse(input);
+
+if (number == 1) 
+{
+    Console.WriteLine("Monday");
+}
+else if(number == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+else if(number == 3)
+{
+    Console.WriteLine("Wednesday"); 
+} 
+else if(number == 4)
+{
+    Console.WriteLine("Thirsday"); 
+} 
+else if(number == 5)
+{
+    Console.WriteLine("Friday");
+} 
+else if(number == 6) 
+{
+    Console.WriteLine("Satturday"); 
+} 
+else if(number == 7)
+{
+    Console.WriteLine("Sunday"); 
+}
+
