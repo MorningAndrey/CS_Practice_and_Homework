fdb341565de25ff26230c7f352bfe5dddf203adc
Electronic <sv_gravity100@mail.ru>
2022-12-17 15:55:08 +0300

    added HOMEWORK_TASK3: домашка к семинару 1 (С#) Задача 4

---
+++ HOMEWORK_TASK3/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите первое целое число: ");
+string input1 = Console.ReadLine() ??"";
+int a = int.Parse(input1);
+
+Console.Write("Введите второе целое число: ");
+string input2 = Console.ReadLine() ??"";
+int b = int.Parse(input2);
+
+Console.Write("Введите третье целое число: ");
+string input3 = Console.ReadLine() ??"";
+int c = int.Parse(input3);
+
+// Наиболее коротким вариантом решения является применение метода Math.Max():
+//int first_comparison = Math.Max(a, b);
+//int result = Math.Max(first_comparison, c);
+
+//Console.WriteLine($"максимальным из трех чисел является число {result}");
+
+// либо ветвление через if:
+int max = a;
+if(a < b)
+{
+    max = b;
+}
+if( max < c)
+{
+    max = c;
+}
+Console.WriteLine($"максимальным из трех чисел является число {max}");
+
