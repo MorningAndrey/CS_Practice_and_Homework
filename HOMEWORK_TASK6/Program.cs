c7b6d7ed83db4275d75d2631bdc66ebd5d066081
Electronic <sv_gravity100@mail.ru>
2022-12-17 21:47:32 +0300

    added HOMEWORK_TASK6: домашка к семинару 1 (С#) Задача 6

---
+++ HOMEWORK_TASK6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine() ??"");
+
+if(number % 2 == 0)
+{
+    Console.WriteLine($"число {number} -> четное");
+}
+else
+{
+    Console.WriteLine($"число {number} -> нечетное");
+}
