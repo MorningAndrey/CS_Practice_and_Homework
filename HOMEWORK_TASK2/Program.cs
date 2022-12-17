d77831f9bf9e79b66405f694b3087dd4ae68c539
Electronic <sv_gravity100@mail.ru>
2022-12-17 14:51:10 +0300

    added HOMEWORK_ TASK2: Домашка по Семинару 1(C#) Задача 2

---
+++ HOMEWORK_TASK2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое целое число: ");
+string input1 = Console.ReadLine() ??"";
+int a = int.Parse(input1);
+
+Console.Write("Введите второе целое число: ");
+string input2 = Console.ReadLine() ??"";
+int b = int.Parse(input2);
+
+//можно решить задачу через метод Math.Max():
+//int result = Math.Max(a,b);  
+//Console.WriteLine($"Максимальным числом является число {result}");
+// или просто через ветвление:
+
+int max = a;
+if(a < b)
+{
+    max = b;
+}
+Console.WriteLine($"Максимальным числом является число {max}");
