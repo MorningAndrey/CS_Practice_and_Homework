42d32e11adfe574c4eb49d757bdff0b359775dc9
Electronic <sv_gravity100@mail.ru>
2022-12-21 01:04:45 +0300

    Seminar_2_Task_2: программа, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа

---
+++ Seminar_2_EX02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a2 = num % 100;
+int a3 = a2 % 10;
+
+int result = a1 * 10 + a3;
+
+Console.WriteLine($"{num} - > {result}");
+
