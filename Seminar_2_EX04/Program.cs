eaac9b16def85b6dc94e0998a47f1a3a013196ad
Electronic <sv_gravity100@mail.ru>
2022-12-21 01:42:25 +0300

    Seminar_2_task_4: программа, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23

---
+++ Seminar_2_EX04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine()  ??"");
+
+int condition1 = num % 7;
+int condition2 = num % 23;
+if (condition1 == 0 && condition2 == 0)
+    Console.Write($"{num} - > да");
+else
+    Console.Write($"{num} - > нет");
