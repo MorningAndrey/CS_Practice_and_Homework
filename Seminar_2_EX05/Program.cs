3f98e5db522266bc5c26b628ea66b3a96f774a1e
Electronic <sv_gravity100@mail.ru>
2022-12-21 01:55:36 +0300

    Seminar_2_Task_5: программа, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого. Пример: 25,5->да. 78,7-> нет.

---
+++ Seminar_2_EX05/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите первое целое число: ");
+int a1 = int.Parse(Console.ReadLine() ??"");
+
+Console.Write("Введите второе целое число: ");
+int a2 = int.Parse(Console.ReadLine() ??"");
+
+int A2 = a1*a1;
+int A1 = a2*a2;
+
+if (A1 == a1 || A2 == a2)
+    Console.Write($"{a1},{a2} -> да");
+else
+    Console.Write($"{a1},{a2} -> нет");
