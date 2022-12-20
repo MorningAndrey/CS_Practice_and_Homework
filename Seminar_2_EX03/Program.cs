8a951940c033a68d01d781a937e78896a4dd43bc
Electronic <sv_gravity100@mail.ru>
2022-12-21 01:25:55 +0300

    Seminar_2_Task_3: программа, которая принимает на вход два числа и выводит, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления

---
+++ Seminar_2_EX03/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int userInput1 = int.Parse(Console.ReadLine() ??"");
+
+Console.Write("Введите второе число: ");
+int userInput2 = int.Parse(Console.ReadLine() ??"");
+
+int dev = userInput1 % userInput2;
+
+if (dev == 0)
+    Console.Write("кратно");
+else
+    Console.Write($"- > не кратно, остаток {dev} ");
+
+
+
