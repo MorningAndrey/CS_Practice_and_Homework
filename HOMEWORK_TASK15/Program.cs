316db697b7cd80f37fa3213b4e517583aac912b3
Electronic <sv_gravity100@mail.ru>
2022-12-21 17:08:58 +0300

    Seminar_2_Task15: программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. Пример: 7 -> да. 1 -> нет.

---
+++ HOMEWORK_TASK15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int day = int.Parse(Console.ReadLine() ??"");
+
+if( day > 5)
+    Console.Write($"{day} -> да");
+else
+    Console.Write($"{day} -> нет");
+    
