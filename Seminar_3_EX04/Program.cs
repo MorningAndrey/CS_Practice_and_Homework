372fc42de859b630d72ef63f7663cc79286e031f
Electronic <sv_gravity100@mail.ru>
2022-12-26 22:54:55 +0300

    программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N. 5 -> 1, 4, 9, 16, 25.

---
+++ Seminar_3_EX04/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int N =0;
+try
+{
+    Console.Write("Введите целое число: ");
+    N = int.Parse(Console.ReadLine() ??"");
+    N = Math.Abs(N);
+}
+catch(Exception exc)
+{
+    Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+}
+
+Console.Write($"{N} -> ");
+
+for(int count = 1; count < N; count++)
+{
+    Console.Write($"{Math.Pow(count, 2)},");
+}
+Console.Write($"{Math.Pow(N, 2)}.");
