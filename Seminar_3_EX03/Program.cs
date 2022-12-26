091c6d0e75938f4ad2e88d6267bc721ddcfdfa80
Electronic <sv_gravity100@mail.ru>
2022-12-26 22:01:15 +0300

    программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве. A (3,6); B (2,1) -> 5,09 A (7,-5); B (1,-1) -> 7,21

---
+++ Seminar_3_EX03/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите координаты x1: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты x2: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты y1: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+double result  = Math.Round(distance, 2);
+
+Console.WriteLine($"Расстояние между двумя точками на плоскости равно {result}");
+
+
+
+
