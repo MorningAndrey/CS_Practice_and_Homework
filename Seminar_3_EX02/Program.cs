11c12c873015f8b297d1fc00ec3f026b51ef5acc
Electronic <sv_gravity100@mail.ru>
2022-12-26 21:57:58 +0300

    программа, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)

---
+++ Seminar_3_EX02/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+int num;
+
+try
+{
+    Console.Write("Введите номер четверти от 1 до 4: ");
+    num = int.Parse(Console.ReadLine() ?? "");
+}
+catch (Exception exc)
+{
+    Console.WriteLine($"Ошибка ввода! {exc.Message}");
+    return;
+}
+
+if (num == 1) 
+{
+    Console.WriteLine("X > 0 && Y > 0");
+}
+else if (num == 2) 
+{
+    Console.WriteLine("X < 0 && Y > 0");
+}
+else if (num == 3)
+{
+    Console.WriteLine("X < 0 && Y < 0");
+}
+else if (num == 4)
+{
+    Console.WriteLine("X > 0 && Y < 0");
+}
+else
+{
+    Console.WriteLine("Ошибка! Точка попадает на оси координат!");
+}
