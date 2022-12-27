2b85d96e6b1de11dd65c45a750e669dd86abe021
Electronic <sv_gravity100@mail.ru>
2022-12-27 13:24:29 +0300

    Seminar_3_TASK23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.

---
+++ HOMEWORK_TASK23/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int N;
+int n;
+while (true)
+{
+        Console.Write("Введите целое число: ");
+        if (int.TryParse(Console.ReadLine(), out N))
+            break;
+        Console.WriteLine("Ошибка ввода!");
+}
+n = Math.Abs(N);
+Console.Write($"{N} -> ");
+
+for(int count = 1; count < n; count++)
+{
+    Console.Write($" {Math.Pow(count, 3)}, ");
+}
+Console.Write($" {Math.Pow(n, 3)}. ");
