63b648124718a6297ecefcc3ce61dd47fe206576
Electronic <sv_gravity100@mail.ru>
2023-01-16 03:03:19 +0300

    Seminar_4_Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.

---
+++ Seminar_4_EX02/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+int num = CorrectUserInput("Введите число: ", "Ошибка ввода");
+int figureNumbers = GetFigureNumbers(num);
+Console.WriteLine($"{num} -> {figureNumbers}");
+
+
+int CorrectUserInput(string number, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(number);
+        bool correctNumber = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(correctNumber)
+        return userNumber;
+        Console.Write(errorMessage);
+    }
+}
+
+int GetFigureNumbers(int result)
+{
+    int count = 0;
+    while(result > 0)
+    {
+        result = result / 10;
+        count++;
+    }
+    return count;
+}
