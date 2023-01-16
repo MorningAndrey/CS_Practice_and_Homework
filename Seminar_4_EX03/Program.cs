d0cd3023116d072a5b98e5ecebd7205e9178bb2d
Electronic <sv_gravity100@mail.ru>
2023-01-16 14:01:37 +0300

    Seminar_4_Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.

---
+++ Seminar_4_EX03/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+Console.Clear();
+int num = CorrectNumber("Введите число N: ", "Ошибка ввода");
+int multiplication = GetResult(num);
+Console.WriteLine($" {num} -> {multiplication}");
+
+int CorrectNumber(string number, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(number);
+        bool correctNumber = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (correctNumber)
+            return userNumber;
+        Console.Write(errorMessage);
+    }
+}
+
+int GetResult(int result)
+{
+    int m = 1;
+    for (int i = 1; i <= result; i++)
+    {
+        m = m * i;
+    }
+    return m;
+}
