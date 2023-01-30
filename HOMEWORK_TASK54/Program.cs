1daa27c74dbc80546415c1c260ef0f3211237835
Electronic <sv_gravity100@mail.ru>
2023-01-30 18:37:24 +0300

    Seminar_8_Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.

---
+++ HOMEWORK_TASK54/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 54:
+// Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+//-------------------------- Начало программы ----------------------------------
+Console.Clear();
+Console.WriteLine("Задача 54:Задайте двумерный массив. Напишите программу,которая упорядочит по убыванию элементы каждой строки двумерного массива.");
+Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
+int rows = GetNumberFromUser("Введите количество строк массива: ", "Ошибка ввода!");
+int columns = GetNumberFromUser("Введите количество столбцов массива: ", "Ошибка ввода!");
+int range = GetNumberFromUser("Введите диапазон значений элементов: от 1 до ", "Ошибка ввода!");
+int[,] array = new int[rows, columns];
+GetArray(array);
+PrintArray(array);
+Console.WriteLine($"\nОтсортированный массив: ");
+SelectionSort(array);
+PrintArray(array);
+// -------------------------- Конец программы ----------------------------------
+// -------------------------Определение методов ---------------------------------
+// ------------------------------------------------------------------------------
+// Метод запроса числа у пользователя сс проверкой на ошибки ввода
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+// метод определения двумерного массива целых чисел
+void GetArray(int[,] result)
+{
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(range);
+        }
+    }
+}
+// метод вывода массива в консоль
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SelectionSort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int m = 0; m < array.GetLength(1) -1; m++)
+            {
+                if (array[i, m] < array[i, m + 1])
+                {
+                    int temp = array[i, m + 1];
+                    array[i, m + 1] = array[i, m];
+                    array[i, m] = temp;
+                }
+            }
+        }
+    }
+}
