266ac74afa5b30f16ff018a56fe19aa8b8166f55
Electronic <sv_gravity100@mail.ru>
2023-02-05 03:56:25 +0300

    Seminar_9_Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.

--- HOMEWORK_TASK64/Program.cs
+++ HOMEWORK_TASK64/Program.cs
@@ -1,23 +1,15 @@
-﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
-// M = 1; N = 5 -> "1, 2, 3, 4, 5"
-// M = 4; N = 8 -> "4, 6, 7, 8"
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 //-------------------------- Начало программы ----------------------------------
 Console.Clear();
-int m=GetNumberFromUser($"Введите число ", "Ошибка ввода!");  //  вызов метода
-int n=GetNumberFromUser($"Введите число ", "Ошибка ввода!");  //  вызов метода
-Console.Write($"M = {m}; N = {n}");
-int temp = m;
-if (m > n)
-{
-    m = n;
-    n = temp;
-}
-if(m == n) Console.Write($" -> '{m}' ");
-else 
-{
-    string result = GetResult(m, n);
-    Console.Write($" -> '{result}' ");
-}
+int n = GetNumberFromUser($"Введите число N: ", "Ошибка ввода!");
+int count = 2;
+Console.Write($"N = {n} -> ");
+Console.Write("'");
+GetResult(n, count);
+Console.Write(1);
+Console.Write("'");
 // -------------------------- Конец программы ----------------------------------
 // -------------------------Определение методов ---------------------------------
 // ------------------------------------------------------------------------------
@@ -33,9 +25,10 @@ int GetNumberFromUser(string message, string errorMessage)
         Console.WriteLine(errorMessage);
     }
 }
-// Метод рекурсивного определения натуральных чисел в промежутке от M до N
-string GetResult (int start, int end)
+// метод определения натуральных чисел в промежутке от N до 1
+void GetResult(int n, int count)
 {
-    if(start == end) return end.ToString();
-    else return start  + " " + GetResult(start +1, end);
+    if (count > n) return;
+    GetResult(n, count + 1);
+    Console.Write(count + ", ");
 }
