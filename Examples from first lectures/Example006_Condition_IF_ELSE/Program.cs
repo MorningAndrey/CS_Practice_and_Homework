59cb4d92ab3300f445e096963cdb1ed943c0df4b
Electronic <sv_gravity100@mail.ru>
2023-02-15 09:40:08 +0300

    Added some simple examples from the very first lectures of C#

---
+++ Examples from first lectures/Example006_Condition_IF_ELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine()!;
+
+if(username.ToLower() == "spongebob")
+{
+    Console.WriteLine("Who lives in a pineapple under the sea? SpongeBob SquarePants!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
---
+++ Examples from first lectures/Example012_The 2nd_lecture_find_number_of_array_by_index/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 1, 2, 3, 53, 5, 6, 67, 8, 9, 45, 67, 53 };
+
+int n = array.Length;
+int find = 53;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
